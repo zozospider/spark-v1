63536180276426ede4206464afbfdceab429d062
zozospider <2642220896@qq.com>
2021-03-16 19:51:02 +0800

    update spark-v1

---
+++ src/main/scala/com/zozospider/spark/core/rdd/operator/serializable/RDDOperator02Kryo.scala
@@ -0,0 +1,16 @@
+package com.zozospider.spark.core.rdd.operator.serializable
+
+// 序列化 - Kryo 序列化框架
+object RDDOperator02Kryo {
+
+  def main(args: Array[String]): Unit = {
+
+    // 参考地址: https://github.com/EsotericSoftware/kryo
+
+    // Java 的序列化能够序列化任何的类. 但是比较重 (字节多), 序列化后, 对象的提交也比较大
+    // Spark 出于性能的考虑, Spark 2.0 开始支持另外一种 Kryo 序列化机制, Kryo 速度是 Serializable 的 10 倍
+    // 当 RDD 在 Shuffle 数据的时候, 简单数据类型, 数组和字符串类型已经在 Spark 内部使用 Kryo 来序列化
+
+  }
+
+}
