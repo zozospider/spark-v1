66fb01b1a11bb2b87b821597ba592eaee14a21cc
zozospider <2642220896@qq.com>
2021-04-09 23:49:07 +0800

    update spark-v1

---
+++ src/main/scala/com/zozospider/spark/streaming/dstream/create/DStreamCreate03Kafka.scala
@@ -0,0 +1,10 @@
+package com.zozospider.spark.streaming.dstream.create
+
+// DStream 创建 - Kafka 数据源
+object DStreamCreate03Kafka {
+
+  def main(args: Array[String]): Unit = {
+    // TODO
+  }
+
+}
