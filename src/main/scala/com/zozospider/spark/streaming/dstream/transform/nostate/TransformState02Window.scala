342e11cd513aa311123873a85da2f25912d3f6b4
zozospider <2642220896@qq.com>
2021-04-10 22:29:01 +0800

    update spark-v1

--- src/main/scala/com/zozospider/spark/streaming/dstream/transform/nostate/TransformState02Window.scala
+++ src/main/scala/com/zozospider/spark/streaming/dstream/transform/nostate/TransformState02Window.scala
@@ -10,6 +10,8 @@ object TransformState02Window {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("Streaming").setMaster("local[*]")
     val streamingContext: StreamingContext = new StreamingContext(conf = conf, batchDuration = Seconds(3))
+    // 采用 E 方式时需要指定 checkpoint 目录
+    streamingContext.checkpoint("checkpoint")
 
     // Window Operations 可以设置窗口的大小和滑动窗口的间隔来动态的获取当前 Steaming 的允许状态
     // 所有基于窗口的操作都需要两个参数, 分别为窗口时长以及滑动步长
@@ -21,24 +23,37 @@ object TransformState02Window {
     val dStream: DStream[String] = inputDStream.flatMap(_.split(" "))
     val dStream2: DStream[(String, Int)] = dStream.map((s: String) => (s, 1))
 
-    // A. 窗口的范围应该是采集周期的整数倍
-    // 3 秒一个批次, 窗口 12 秒 (存在重复统计)
+    // A. 3 秒一个批次, 窗口 12 秒 (有重复统计)
     /*val dStreamA: DStream[(String, Int)] = dStream2.window(windowDuration = Seconds(12))
     val dStreamA2: DStream[(String, Int)] = dStreamA.reduceByKey((i1: Int, i2: Int) => i1 + i2)
     dStreamA2.print*/
 
-    // B. 窗口可以滑动, 但是默认情况下, 一个采集进行滑动, 这样的话可能会出现重复数据的计算, 为了避免这种情况, 可以改变滑动的步长 (如窗口和滑步都为 6s)
-    // 3 秒一个批次, 窗口 12 秒, 滑步 6 秒 (存在重复统计)
-    /*val dStreamB: DStream[(String, Int)] = dStream2.window(windowDuration = Seconds(12), slideDuration = Seconds(6))
+    // B. 3 秒一个批次, 窗口 6 秒, 滑步 6 秒 (没有重复统计)
+    // 窗口可以滑动, 但是默认情况下, 一个采集进行滑动, 这样的话可能会出现重复数据的计算, 为了避免这种情况, 可以改变滑动的步长 (如窗口和滑步都为 6s)
+    /*val dStreamB: DStream[(String, Int)] = dStream2.window(windowDuration = Seconds(6), slideDuration = Seconds(6))
     val dStreamB2: DStream[(String, Int)] = dStreamB.reduceByKey((i1: Int, i2: Int) => i1 + i2)
     dStreamB2.print*/
 
-    // C. 与 B 效果一样
-    // 3 秒一个批次, 窗口 12 秒, 滑步 6 秒 (存在重复统计)
-    val dStreamC: DStream[(String, Int)] = dStream2.reduceByKeyAndWindow(reduceFunc = {
+    // C. 3 秒一个批次, 窗口 12 秒, 滑步 6 秒 (有重复统计)
+    /*val dStreamC: DStream[(String, Int)] = dStream2.window(windowDuration = Seconds(12), slideDuration = Seconds(6))
+    val dStreamC2: DStream[(String, Int)] = dStreamC.reduceByKey((i1: Int, i2: Int) => i1 + i2)
+    dStreamC2.print*/
+
+    // D. 3 秒一个批次, 窗口 12 秒, 滑步 6 秒 (有重复统计)
+    /*val dStreamD: DStream[(String, Int)] = dStream2.reduceByKeyAndWindow(reduceFunc = {
       (i1: Int, i2: Int) => i1 + i2
     }, windowDuration = Seconds(12), slideDuration = Seconds(6))
-    dStreamC.print
+    dStreamD.print*/
+
+    // E. 3 秒一个批次, 窗口 12 秒, 滑步 6 秒 (有重复统计)
+    // 当窗口较大, 滑步较小时, 可以采用增加数据和删除数据的方式, 无需重复计算 (此处不是重复统计), 提示性能
+    // 注: 此方式需要指定 checkpoint 目录
+    val dStreamE: DStream[(String, Int)] = dStream2.reduceByKeyAndWindow(reduceFunc = {
+      (i1: Int, i2: Int) => i1 + i2
+    }, invReduceFunc = {
+      (i1: Int, i2: Int) => i1 - i2
+    }, windowDuration = Seconds(12), slideDuration = Seconds(6))
+    dStreamE.print
 
     streamingContext.start
     streamingContext.awaitTermination
