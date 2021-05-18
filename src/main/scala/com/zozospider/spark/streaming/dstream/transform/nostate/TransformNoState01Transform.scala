c376df238ffe18fc47f4a0a4c9f56aa63b41e6de
zozospider <2642220896@qq.com>
2021-05-18 15:36:06 +0800

    update spark-v1

--- src/main/scala/com/zozospider/spark/streaming/dstream/transform/nostate/TransformNoState01Transform.scala
+++ src/main/scala/com/zozospider/spark/streaming/dstream/transform/nostate/TransformNoState01Transform.scala
@@ -2,7 +2,7 @@ package com.zozospider.spark.streaming.dstream.transform.nostate
 
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 // DStream 转换 - 无状态转化操作 - transform()
@@ -19,17 +19,51 @@ object TransformNoState01Transform {
     val inputDStream: ReceiverInputDStream[String] = streamingContext.socketTextStream(hostname = "localhost", port = 9999)
 
     // transform() 方法可以将底层 rdd 获取到后进行操作
-    // 1. DStream 功能不完善
+    // transform() 使用场景:
+    // 1. DStream 功能不完善时
     // 2. 需要代码周期性执行
 
     // code: Driver 端执行
-    inputDStream.transform((rdd: RDD[String]) => {
-      // code: Driver 端执行 (周期性执行)
-      rdd.map((s: String) => s)
-    })
+    val dStream: DStream[(String, Int)] = inputDStream.transform(
+
+      // code: Driver 端执行 (周期性执行, 一个采集周期执行一次)
+      (rdd: RDD[String]) => {
+
+        val rdd2: RDD[String] = rdd.flatMap(
+          // code: Executor 端执行
+          (s: String) => s.split(" ")
+        )
+        val rdd3: RDD[(String, Int)] = rdd2.map(
+          // code: Executor 端执行
+          (s: String) => (s, 1)
+        )
+        val rdd4: RDD[(String, Int)] = rdd3.reduceByKey(
+          // code: Executor 端执行
+          (i1: Int, i2: Int) => i1 + i2
+        )
+        rdd4
+      }
+    )
+    dStream.print()
+
+    print("------")
 
     // code: Driver 端执行
-    inputDStream.map((s: String) => s)
+    val dStream2: DStream[String] = inputDStream.flatMap(
+      // code: Executor 端执行
+      (s: String) => s.split(" ")
+    )
+    // code: Driver 端执行
+    val dStream3: DStream[(String, Int)] = dStream2.map(
+      // code: Executor 端执行
+      (s: String) => (s, 1)
+    )
+    // code: Driver 端执行
+    val dStream4: DStream[(String, Int)] = dStream3.reduceByKey(
+      // code: Executor 端执行
+      (i1: Int, i2: Int) => i1 + i2
+    )
+    dStream4.print()
 
     streamingContext.start
     streamingContext.awaitTermination
