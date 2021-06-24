f7ff6a749a616c6bdbb8d4492edfafa3ee694134
zozospider <2642220896@qq.com>
2021-06-24 18:56:46 +0800

    update spark-v1

--- src/main/scala/com/zozospider/spark/core/rdd/operator/transform/RDDOperator23Join2ByBroadcast.scala
+++ src/main/scala/com/zozospider/spark/core/rdd/operator/transform/RDDOperator23Join2ByBroadcast.scala
@@ -17,40 +17,44 @@ object RDDOperator23Join2ByBroadcast {
       seq = List(("a", 100), ("b", 200), ("a", 1000), ("d", 400)))
 
     // 广播变量
-    val array2: Array[(String, Int)] = rddB.collect
-    val broadcast2: Broadcast[Array[(String, Int)]] = context.broadcast(array2)
+    val arrayB: Array[(String, Int)] = rddB.collect
+    val broadcastB: Broadcast[Array[(String, Int)]] = context.broadcast(arrayB)
 
-    val rdd2: RDD[List[(String, (Int, Int))]] = rddA.map((tuple: (String, Int)) => {
-      var result: List[(String, (Int, Int))] = List()
+    val rdd2: RDD[List[(String, (Int, Int))]] = rddA.map((tupleA: (String, Int)) => {
       // 访问广播变量
-      val array: Array[(String, Int)] = broadcast2.value
-      array.foreach((tp: (String, Int)) => {
-        if (tp._1 == tuple._1) {
-          result = result :+ (tuple._1, (tuple._2, tp._2))
+      val arrayB: Array[(String, Int)] = broadcastB.value
+      // 找出 rddB (广播变量) 中具有和 rddA 相同的所有 key, 组合成 tuple 并添加到 list 中
+      var result: List[(String, (Int, Int))] = List()
+      arrayB.foreach((tupleB: (String, Int)) => {
+        if (tupleB._1 == tupleA._1) {
+          result = result :+ (tupleA._1, (tupleA._2, tupleB._2))
         }
       })
       result
     })
+    // list(tuple) 转换成 tuple
     val rdd22: RDD[(String, (Int, Int))] = rdd2.flatMap((list: List[(String, (Int, Int))]) => list)
     rdd22.collect.foreach(println)
 
     println("------")
 
     // 广播变量
-    val array3: Array[(String, Int)] = rddA.collect
-    val broadcast3: Broadcast[Array[(String, Int)]] = context.broadcast(array3)
+    val arrayA: Array[(String, Int)] = rddA.collect
+    val broadcastA: Broadcast[Array[(String, Int)]] = context.broadcast(arrayA)
 
-    val rdd3: RDD[List[(String, (Int, Int))]] = rddB.map((tuple: (String, Int)) => {
-      var result: List[(String, (Int, Int))] = List()
+    val rdd3: RDD[List[(String, (Int, Int))]] = rddB.map((tupleB: (String, Int)) => {
       // 访问广播变量
-      val array: Array[(String, Int)] = broadcast3.value
-      array.foreach((tp: (String, Int)) => {
-        if (tp._1 == tuple._1) {
-          result = result :+ (tuple._1, (tuple._2, tp._2))
+      val arrayA: Array[(String, Int)] = broadcastA.value
+      // 找出 rddA (广播变量) 中具有和 rddB 相同的所有 key, 组合成 tuple 并添加到 list 中
+      var result: List[(String, (Int, Int))] = List()
+      arrayA.foreach((tupleA: (String, Int)) => {
+        if (tupleA._1 == tupleB._1) {
+          result = result :+ (tupleB._1, (tupleB._2, tupleA._2))
         }
       })
       result
     })
+    // list(tuple) 转换成 tuple
     val rdd33: RDD[(String, (Int, Int))] = rdd3.flatMap((list: List[(String, (Int, Int))]) => list)
     rdd33.collect.foreach(println)
 
